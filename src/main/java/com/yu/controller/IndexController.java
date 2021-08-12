c5d0ed52f8d1a508c67db603c7656ab9f2283016
yrz4869 <77534711+yrz4869@users.noreply.github.com>
2021-08-12 16:03:12 +0800

    Create IndexController.java

---
+++ src/main/java/com/yu/controller/IndexController.java
@@ -0,0 +1,29 @@
+package com.yu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+   /*@RequestMapping({"/","/index.html"})
+    public String index(){
+        return "index";
+    }
+   */
+
+    /*@GetMapping({"/property.html"})
+    public String index2(){
+        return "property";
+    }*/
+
+    /*@GetMapping({"/services.html"})
+    public String index3(){
+        return "services";
+    }*/
+    /*
+    @GetMapping({"/services.html"})
+    public String index4(){
+        return "services";
+    }*/
+}
